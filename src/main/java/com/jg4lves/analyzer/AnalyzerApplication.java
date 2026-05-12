c92806fecfa78792b0980e5dc214ec199f38d804
jgalves <hermesjgcode@gmail.com>
2026-05-12 15:23:54 -0300

    feat: initial commit

---
+++ src/main/java/com/jg4lves/analyzer/AnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.jg4lves.analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jg4lves/analyzer/AnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jg4lves.analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
